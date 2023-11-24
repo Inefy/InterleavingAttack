8626e7099ccb731549ce79c9b3c1ae6774e3eab3
rashodk <rmanamperiko@mun.ca>
2023-11-23 21:50:43 -0330

    MFA

--- userlogin/src/components/multiFactorAuth.jsx
+++ userlogin/src/components/multiFactorAuth.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { getAuth, PhoneAuthProvider, MultiFactorSession, MultiFactorAssertion, enrollMultiFactor } from 'firebase/auth';
+import { multiFactor, PhoneAuthProvider, PhoneMultiFactorGenerator,
+  RecaptchaVerifier } from 'firebase/auth';
+
+import auth from '../firebase';
 
 const MultiFactorAuth = () => {
   const [phoneNumber, setPhoneNumber] = useState('');
@@ -8,7 +11,32 @@ const MultiFactorAuth = () => {
   const navigate = useNavigate();
 
   const handleEnableMFA = async () => {
+    // Get the user.
     
+    const user = auth.currentUser;
+
+    const recaptchaVerifier = new RecaptchaVerifier('recaptcha-container-id', undefined, auth);
+    multiFactor(user).getSession()
+    .then(function (multiFactorSession) {
+        // Specify the phone number and pass the MFA session.
+        const phoneInfoOptions = {
+            phoneNumber: phoneNumber,
+            session: multiFactorSession
+        };
+
+        const phoneAuthProvider = new PhoneAuthProvider(auth);
+
+        // Send SMS verification code.
+        return phoneAuthProvider.verifyPhoneNumber(phoneInfoOptions, recaptchaVerifier);
+    }).then(function (verificationId) {
+        // Ask user for the verification code. Then:
+        const cred = PhoneAuthProvider.credential(verificationId, verificationCode);
+        const multiFactorAssertion = PhoneMultiFactorGenerator.assertion(cred);
+
+        // Complete enrollment.
+        return multiFactor(user).enroll(multiFactorAssertion, 'phone number label');
+    });
+
   };
 
   return (
