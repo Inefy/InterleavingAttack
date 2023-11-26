30caa354d653c8bba861c442be6995d8eebce8ba
Zac Batten <58842226+Inefy@users.noreply.github.com>
2023-11-26 13:27:08 -0330

    updated the firebase for realtime

--- userlogin/src/firebase.js
+++ userlogin/src/firebase.js
@@ -1,10 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth";
-
-import firebase from "firebase/compat/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -17,12 +14,13 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const firebaseapp = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig);
 
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(firebaseApp);
 
-const auth = getAuth(firebaseapp);
-// auth.tenantId = "interleaving-attack-o112i";
+// Initialize Firestore and get a reference to the service
+const db = getFirestore(firebaseApp);
 
-
-
-export  {auth};
+export { auth, db };
+ß
