2a5dcfc0bc267b0353e7e36d488b52abfcf7c5a7
Zac Batten <58842226+Inefy@users.noreply.github.com>
2023-11-26 15:00:22 -0330

    adjusted userlogin

--- userlogin/src/components/userLogin.jsx
+++ userlogin/src/components/userLogin.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import {
   signInWithEmailAndPassword,
@@ -6,9 +6,7 @@ import {
   PhoneMultiFactorGenerator,
   PhoneAuthProvider,
   RecaptchaVerifier,
-
 } from 'firebase/auth';
-
 import { auth } from '../firebase';
 
 auth.languageCode = 'en';
@@ -19,61 +17,28 @@ const UserLogin = () => {
   const [verificationCode, setVerificationCode] = useState('');
   const [verificationMessage, setVerificationMessage] = useState(null);
   const navigate = useNavigate();
+  const ws = new WebSocket('ws://localhost:3001'); // Establish WebSocket connection
 
-  auth.settings.appVerificationDisabledForTesting=false;
-  // console.log(auth.settings.appVerificationDisabledForTesting);
-  const handleLogin = async () => {
-    const selectedIndex = 0;
+  useEffect(() => {
+    ws.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      setUsername(data.username);
+      setPassword(data.password);
+    };
 
-    const recaptchaVerifier = await new RecaptchaVerifier(auth,'recaptcha-container',{"size":"invisible"});
-    signInWithEmailAndPassword(auth, username, password)
-    .then(function (userCredential) {
-        // User is not enrolled with a second factor and is successfully
-        // signed in.
-        // ...
-        navigate('/userprofile');
-    })
-    .catch(function (error) {
-        if (error.code === 'auth/multi-factor-auth-required') {
-            const resolver = getMultiFactorResolver(auth, error);
-            // Ask user which second factor to use.
-            if (resolver.hints[selectedIndex].factorId ===
-                PhoneMultiFactorGenerator.FACTOR_ID) {
-                const phoneInfoOptions = {
-                    multiFactorHint: resolver.hints[selectedIndex],
-                    session: resolver.session
-                };
-                const phoneAuthProvider = new PhoneAuthProvider(auth);
-                // Send SMS verification code
-                return phoneAuthProvider.verifyPhoneNumber(phoneInfoOptions, recaptchaVerifier)
-                    .then(function (verificationId) {
-                        // Ask user for the SMS verification code. Then:
-                        setVerificationMessage("Please check your phone for the verification code.");
-                        const verificationCode = window.prompt('Enter the verification code that was sent to your phone:');
+    return () => {
+      ws.close();
+    };
+  }, []);
 
-
-                        const cred = PhoneAuthProvider.credential(
-                            verificationId, verificationCode);
-                        const multiFactorAssertion =
-                            PhoneMultiFactorGenerator.assertion(cred);
-                        // Complete sign-in.
-                        return resolver.resolveSignIn(multiFactorAssertion)
-                    })
-                    .then(function (userCredential) {
-                        // User successfully signed in with the second factor phone number.
-                        navigate('/userprofile');
-                    });
-            } else {
-                // Unsupported second factor.
-
-                recaptchaVerifier.clear();
-            }
-        } else if (error.code === 'auth/wrong-password') {
-            // Handle other errors such as wrong password.
-        }else{
-          console.log(error);
-        }
-    });
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, username, password);
+      navigate('/userprofile');
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
@@ -82,32 +47,25 @@ const UserLogin = () => {
       <div className="bg-white rounded-lg p-8 shadow-lg">
         <h1 className="text-2xl mb-4">User Login</h1>
         {verificationMessage && <p className="mb-4 text-blue-500">{verificationMessage}</p>}
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          className="w-full py-2 px-3 rounded-lg mb-4 border-none bg-gray-200"
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          className="w-full py-2 px-3 rounded-lg mb-4 border-none bg-gray-200"
-        />
-        {/* {verificationMessage && (
+        <form onSubmit={handleLogin}>
           <input
             type="text"
-            placeholder="Verification Code"
-            value={verificationCode}
-            onChange={(e) => setVerificationCode(e.target.value)}
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
             className="w-full py-2 px-3 rounded-lg mb-4 border-none bg-gray-200"
           />
-        )} */}
-        <button onClick={handleLogin} className="w-full py-3 bg-black text-white rounded-lg mb-2">
-          Login
-        </button>
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="w-full py-2 px-3 rounded-lg mb-4 border-none bg-gray-200"
+          />
+          <button type="submit" className="w-full py-3 bg-black text-white rounded-lg mb-2">
+            Login
+          </button>
+        </form>
         <p className="text-center text-gray-500 mb-2">
           <Link to="/forgotpassword" className="text-blue-500">
             Forgot Password?
