8b2a217017969519449c1c8402ae343f7863b8ba
Zac Batten <58842226+Inefy@users.noreply.github.com>
2023-11-26 14:28:08 -0330

    Added listening code

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+let clients = [];
+
+wss.on('connection', (ws) => {
+  clients.push(ws);
+
+  ws.on('message', (message) => {
+    clients.forEach(client => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+
+  ws.on('close', () => {
+    clients = clients.filter(client => client !== ws);
+  });
+});
+
+server.listen(3001, () => {
+  console.log('Server is running on port 3001');
+});
